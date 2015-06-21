d081e4e330824079d888d57cdab9bc2916dca780
yshiga <yuichi.shiga@gmail.com>
2015-06-21 17:14:19 +0900

    fix non-login user error

--- qa-ba-reminder-widget.php
+++ qa-ba-reminder-widget.php
@@ -10,7 +10,11 @@ class qa_ba_reminder_widget {
 	}
 	
 	function output_widget($region, $place, $themeobject, $template, $request, $qa_content) {
-		$list = $this->getNoBestAnswerQuestion();
+		$userid = qa_get_logged_in_userid();
+		if(!isset($userid)){
+			return;
+		}
+		$list = $this->getNoBestAnswerQuestion($userid);
 		if(count($list) === 0){
 			return;
 		}
@@ -25,12 +29,12 @@ class qa_ba_reminder_widget {
 		
 	}
 
-	function getNoBestAnswerQuestion() {
+	function getNoBestAnswerQuestion($userid) {
 
 
 		$sql = "select t1.postid as qid, t1.title, count(t1.postid) as answer_num " . 
 			"from qa_posts t1  join qa_posts t2 on t1.postid = t2.parentid ". 
-			"where t1.userid = " . qa_get_logged_in_userid() . " and t1.type = 'Q' and t1.selchildid is null and t2.type='A' group by t1.postid having answer_num >1";
+			"where t1.userid = " . $userid . " and t1.type = 'Q' and t1.selchildid is null and t2.type='A' group by t1.postid having answer_num >1";
 		$result = qa_db_query_sub($sql); 
 		return qa_db_read_all_assoc($result);
 	}
